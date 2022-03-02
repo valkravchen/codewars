6a3cdf8cca4beaf4d9e09928a696dc094734bedd
Пользователь <kravchenkoval@mail.ru>
2022-03-02 21:02:49 +0300

    add new file

---
+++ Sum of positive.js
@@ -0,0 +1,57 @@
+reer
+Opportunities
+Find your next career challenge – powered by Qualified Jobs
+Community
+Leaderboards
+Achieve honor and move up the global leaderboards
+Chat
+Join our Discord server and chat with your fellow code warriors
+Discussions
+View our Github Discussions board to discuss general Codewars topics
+About
+Docs
+Learn about all of the different aspects of Codewars
+
+    Announcements
+    Read about the latest Codewars updates
+
+    8 kyu
+    2
+
+8 kyu
+Sum of positive
+107031092% of 13,18556,053 of 135,531JbPasquier
+JavaScript
+Train AgainNext Kata
+
+    Details
+    Solutions
+    Forks (12)
+    Discourse (190)
+    Translations
+
+Collect|
+Show Kata Description
+Show Kata Test Cases
+
+    Show Me:
+    All Solutions
+    Solutions of Users I am Following
+
+    Sort By:
+    Best Practices
+    Clever
+    Newest
+    Oldest
+
+    CollinRea, willworth, cwcox, camiloaconcha, doug-kd, julian-Durdur, devosama77, devosma100@gmail.com, enjapurirakesh, AmineL (plus 16 more warriors)
+
+    function positiveSum(arr) {
+      var total = 0;    
+      for (i = 0; i < arr.length; i++) {    
+        if (arr[i] > 0) {                   
+          total += arr[i];                  
+        }
+      }
+      return total;                         
+    }

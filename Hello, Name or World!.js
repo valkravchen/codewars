353bba7de383edad2cea62fca1b7cae5489848dd
valkravchen <100293539+valkravchen@users.noreply.github.com>
2022-06-16 23:38:53 +0300

    Delete Hello, Name or World!.js

--- Hello, Name or World!.js
+++
@@ -1,7 +0,0 @@
-function hello(name){
-    if (name){
-      return "Hello, " + name.substring(0,1).toUpperCase() + name.substring(1).toLowerCase() + '!';
-    } else {
-      return "Hello, World!";
-    }
-  }

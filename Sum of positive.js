4362588fb3698c545ccc73bed0b23fb4818997d7
valkravchen <100293539+valkravchen@users.noreply.github.com>
2022-06-22 20:27:57 +0300

    Delete Sum of positive.js

--- Sum of positive.js
+++
@@ -1,9 +0,0 @@
-function positiveSum(arr) {
-    var total = 0;    
-    for (i = 0; i < arr.length; i++) {    
-      if (arr[i] > 0) {                   
-        total += arr[i];                  
-      }
-    }
-    return total;                         
-  }
